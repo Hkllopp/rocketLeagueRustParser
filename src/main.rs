6abd0c9f9e54058851fc5fa50de736db938462e2
Augustin BAR <baraugustin26@gmail.com>
2023-06-05 10:28:10 +0200

    Adding configuration file

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use boxcars::{ParseError, Replay};
 use std::error;
 use std::fs;
 use std::path::Path;
+use std::collections::HashMap;
+use config::Config;
 
 fn parse_rl(data: &[u8]) -> Result<Replay, ParseError> {
     boxcars::ParserBuilder::new(data)
@@ -9,28 +11,29 @@ fn parse_rl(data: &[u8]) -> Result<Replay, ParseError> {
         .parse()
 }
 
-fn run() -> Result<(), Box<dyn error::Error>> {
-    let replay_dir = "./raw_replays";
-    let json_dir = "./parsed_replays";
+fn run(config_str : HashMap<String, String>) -> Result<(), Box<dyn error::Error>> {
+    let raw_replays_folder = config_str.get("raw_replays_folder").unwrap();
+    let parsed_replays_folder = config_str.get("parsed_replays_folder").unwrap();
+    let replays_extension = config_str.get("replays_extension").unwrap();
 
-    // Créer le dossier ./json s'il n'existe pas
-    fs::create_dir_all(json_dir)?;
+    // Create parsed_replays_folder folder if it doesn't exist
+    fs::create_dir_all(parsed_replays_folder)?;
 
-    // Lire les fichiers .replay du dossier ./replay
-    let replay_files = fs::read_dir(replay_dir)?;
+    // Read the replays_extension files from the raw_replays_folder folder
+    let replay_files = fs::read_dir(raw_replays_folder)?;
     for replay_file in replay_files {
         let replay_file = replay_file?;
         let replay_path = replay_file.path();
         if let Some(extension) = replay_path.extension() {
-            if extension == "replay" {
+            if extension.to_str() == Some(replays_extension) {
                 let replay_filename = replay_path.file_name().unwrap().to_string_lossy();
                 let json_filename = format!("{}.json", replay_filename);
 
-                let json_path = Path::new(json_dir).join(&json_filename);
+                let json_path = Path::new(parsed_replays_folder).join(&json_filename);
 
-                // Vérifier si le fichier JSON existe déjà
+                // Check if the json parsed file already exists
                 if json_path.exists() {
-                    println!("Le fichier {} existe déjà, il sera ignoré.", json_filename);
+                    println!("File {} already parsed, will be ignored", json_filename);
                     continue;
                 }
 
@@ -46,7 +49,19 @@ fn run() -> Result<(), Box<dyn error::Error>> {
     Ok(())
 }
 
-fn main() { 
-    let _ = run();
-    println!("Parsing fini !");
+
+fn load_config() -> HashMap<String, String>{
+    let settings = Config::builder()
+    .add_source(config::File::with_name("config"))
+    .build()
+    .unwrap();
+
+    settings.try_deserialize::<HashMap<String, String>>().unwrap()
+}
+
+fn main(){
+    let config_str =load_config();
+
+    let _ = run(config_str);
+    println!("Parsing done !");
 }
